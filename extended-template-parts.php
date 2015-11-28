7473bb78d35c88cb8e4b0ac4fe5fd00b9c7af8a7
John Blackbourn <johnbillion@gmail.com>
2015-11-28 02:57:53 +0000

    No need to deal with WP's globals, these can be explictly globalised in the template as necessary.

--- extended-template-parts.php
+++ extended-template-parts.php
@@ -97,14 +97,7 @@ class Extended_Template_Part {
 	}
 
 	protected function load_template( $template_file ) {
-
-		global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
-
-		if ( is_array( $wp_query->query_vars ) )
-			extract( $wp_query->query_vars, EXTR_SKIP );
-
 		require $template_file;
-
 	}
 
 	function get_cache() {
