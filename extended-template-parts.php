e02a376f31e15bc57334047ecf3357ad2e4fe25d
John Blackbourn <johnbillion@gmail.com>
2018-09-27 15:06:12 +0100

    Move the function into its own template file and add file autoloading.

--- extended-template-parts.php
+++ extended-template-parts.php
@@ -24,30 +24,4 @@
  * Require class
  */
 require_once __DIR__ . '/class-extended-template-part.php';
-
-/**
- * Output a template part.
- *
- * This function is functionally identical to `get_template_part()`. In addition, it allows you to pass in
- * variables for use in the template part using the `$vars` argument, which will then be available in the
- * `$this->vars` property from within the template part.
- *
- * @param string $slug The slug name for the generic template.
- * @param string $name The name of the specialised template.
- * @param array  $vars Variables for use within the template part.
- * @param array  $args {
- *     Arguments for the template part.
- *
- *     @type int|false $cache The number of seconds this template part should be cached for, or boolean false
- *                            for no caching. Default false.
- *     @type string    $dir   The theme subdirectory to look in for template parts. Default 'template-parts'.
- * }
- */
-function get_extended_template_part( $slug, $name = '', array $vars = [], array $args = [] ) {
-	$template = new Extended_Template_Part( $slug, $name, $vars, $args );
-	$dir = $template->args['dir'];
-	$dir_slug = "{$dir}/{$slug}";
-	/* This action is documented in WordPress core: wp-includes/general-template.php */
-	do_action( "get_template_part_{$dir_slug}", "{$dir_slug}", $name );
-	echo $template->get_output(); // WPCS: XSS ok.
-}
+require_once __DIR__ . '/functions.php';
