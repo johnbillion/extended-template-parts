d40697772a180eab36ec92d7ee97d54bae317faf
John Blackbourn <johnbillion@gmail.com>
2018-09-27 16:16:18 +0100

    Fix the escaped output sniff.

--- functions.php
+++ functions.php
@@ -29,5 +29,5 @@ function get_extended_template_part( string $slug, string $name = '', array $var
 	/* This action is documented in WordPress core: wp-includes/general-template.php */
 	do_action( "get_template_part_{$dir_slug}", "{$dir_slug}", $name );
 
-	echo $template->get_output();
+	echo $template->get_output(); // WPCS: XSS ok.
 }
