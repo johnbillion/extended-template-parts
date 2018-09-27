989f663c3c34de598ea5ad391fdbb345c5aaf995
John Blackbourn <johnbillion@gmail.com>
2018-09-27 17:10:28 +0100

    Version 2.0.0.

--- extended-template-parts.php
+++ extended-template-parts.php
@@ -3,7 +3,7 @@
  * Extended template parts for WordPress.
  *
  * @package   ExtendedTemplateParts
- * @version   1.1.1
+ * @version   2.0.0
  * @author    John Blackbourn <https://johnblackbourn.com>
  * @link      https://github.com/johnbillion/extended-template-parts
  * @copyright 2012-2018 John Blackbourn
